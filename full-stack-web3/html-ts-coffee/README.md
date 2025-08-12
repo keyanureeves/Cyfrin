394340a18ce606e34a6a3c1e9f5eeb79cfc77f27
keyanureeves <osbonkeya@gmail.com>
2025-08-12 03:06:33 +0300

    smple contract-frontend intergration

---
+++ full-stack-web3/html-ts-coffee/README.md
@@ -0,0 +1,10 @@
+<!-- What are we naking? -->
+
+- Minimal HTML/JS site 
+- That has the followwing buttons which map to the solidity smart ccontract:
+    - Connect
+    - Buy Coffee 
+        -Button to buy coffee
+        - Call a function on a ssmart contract
+    -Get balance 
+    -Withdraw 
---
+++ full-stack-web3/html-ts-coffee/index-js.js
@@ -0,0 +1,94 @@
+import {createWalletClient, custom, createPublicClient, parseEther, defineChain} from "https://esm.sh/viem"
+import { contractAddress, abi } from "./constants-js.js";
+
+
+const connectButton = document.getElementById('connectButton');
+const fundButton = document.getElementById('fundButton');
+const ethAmountInput = document.getElementById('ethAmount');
+
+let walletClient
+let publicClient
+let connectedAccount
+
+async function connect() {
+    if (typeof window.ethereum !== 'undefined') {
+         walletClient = createWalletClient({
+            transport:custom(window.ethereum)// we could add a different type of transp
+        })
+
+        const accounts = await walletClient.requestAddresses()
+        connectedAccount = accounts[0]
+        console.log(`Connected account: ${connectedAccount}`);
+    
+    } else {
+        connectButton.innerText = "Please install a wallet extension like MetaMask";
+    }
+}
+
+async function fund() {
+    const ethAmount = ethAmountInput.value;
+    console.log(`Funding with ${ethAmount} ETH`);
+
+     if (typeof window.ethereum !== 'undefined') {
+         walletClient = createWalletClient({
+            transport:custom(window.ethereum)// we could add a different type of transport
+        })
+
+        if (!connectedAccount) { // ✅ Ensure we have an account before funding
+            const accounts = await walletClient.getAddresses();
+            connectedAccount = accounts[0];
+        }
+
+        const currentChain = await getCurrentChain(walletClient);
+
+        publicClient = createPublicClient({
+            transport: custom(window.ethereum)
+        });
+
+        console.log(parseEther(ethAmount));
+
+        // await publicClient.simulateContract({
+        //     //address, abi
+        //     address : contractAddress,
+        //     abi: abi,
+        //     functionName: 'fund',
+        //     account: connectedAccount,
+        //     chain: currentChain, 
+        //     value:parseEther(ethAmount),
+        // })
+
+
+    
+    } else {
+        connectButton.innerText = "Please install a wallet extension like MetaMask";
+    }
+}
+
+async function getCurrentChain  (client){
+    const chainId = await client.getChainId()
+    const currentChain = defineChain({
+        id: chainId,
+        nativeCurrency: {
+            name: 'Ether',
+            symbol: 'ETH',
+            decimals: 18
+        },
+
+        rpcUrls: {
+            default: {
+                https: ["https://localhost:"],
+            },
+        },
+    })
+
+    return currentChain
+}
+
+
+
+
+
+connectButton.onclick = connect;
+fundButton.onclick = fund;
+
+
