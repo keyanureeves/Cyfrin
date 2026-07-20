20333bc59e576c5c7d57018f212930a1f90f9331
keyanureeves <osbonkeya@gmail.com>
2026-07-20 15:18:25 +0300

    stuff

--- Remix-Storage-Factory/SimpleStorage.sol
+++ Remix-Storage-Factory/SimpleStorage.sol
@@ -1,4 +1,3 @@
-
 // SPDX-License-Identifier: MIT
 
 pragma solidity >=0.8.19;
@@ -6,14 +5,11 @@ pragma solidity >=0.8.19;
 // pragma solidity ^0.8.0;
 // pragma solidity >=0.8.0 <0.9.0;
 
-
-
-
 contract SimpleStorage {
     //favouriteNumber gets initiaised to 0 if no value is given
     uint256 myFavoriteNumber;
 
-    //custom data type 
+    //custom data type
     struct Person {
         uint256 favoriteNumber;
         string name;
@@ -29,21 +25,19 @@ contract SimpleStorage {
         myFavoriteNumber = _favoriteNumber;
     }
 
-    //pure and view functions cannot cahnge the state of the blockchain hence cost no gas 
+    //pure and view functions cannot cahnge the state of the blockchain hence cost no gas
     function retrieve() public view returns (uint256) {
         return myFavoriteNumber;
     }
-
-    
 
     function addPerson(string memory _name, uint256 _favoriteNumber) public {
         listOfPeople.push(Person(_favoriteNumber, _name));
         nameToFavoriteNumber[_name] = _favoriteNumber;
     }
-
-
 }
 
-contract SimpleStorage2{}
-contract SimpleStorage3{}
-contract SimpleStorage4{}
+contract SimpleStorage2 {}
+
+contract SimpleStorage3 {}
+
+contract SimpleStorage4 {}
